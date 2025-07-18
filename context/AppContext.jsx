1110ada8ab62a702a89c29de2fa7cc905caee6a1
silpakammara <silpa.kammara@dataspeaks.co>
2025-07-18 20:14:07 +0530

    user data get api create

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,8 +1,11 @@
 'use client'
 import { productsDummyData, userDummyData } from "@/assets/assets";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
+import { set } from "mongoose";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -17,6 +20,8 @@ export const AppContextProvider = (props) => {
 
     const {user}=useUser()
 
+    const {getToken}=useAuth()
+
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
     const [isSeller, setIsSeller] = useState(true)
@@ -27,7 +32,23 @@ export const AppContextProvider = (props) => {
     }
 
     const fetchUserData = async () => {
-        setUserData(userDummyData)
+      try{
+          if(user.publicMetadata.role==="seller"){
+            setIsSeller(true)
+        }
+      const token = await getToken()
+      const {data}=await axios.get('/api/user/data',{headers:{Authorization:`Bearer ${token}`}})
+      if(data.success){
+        setUserData(data.user)
+        setCartItems(data.user.cartItems)
+      }
+      else{
+        toast.error(data.message)
+      }
+    }
+      catch(error){
+        toast.error(error.message)
+      }
     }
 
     const addToCart = async (itemId) => {
@@ -81,11 +102,14 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if(user){
+            fetchUserData()
+        }
+    }, [user])
 
     const value = {
         user,
+        getToken,
         currency, router,
         isSeller, setIsSeller,
         userData, fetchUserData,
