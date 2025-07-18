0e7a08c3c3f11102566026932767e6e3aad03387
silpakammara <silpa.kammara@dataspeaks.co>
2025-07-18 18:00:16 +0530

    function error change

--- config/inngest.js
+++ config/inngest.js
@@ -16,12 +16,12 @@ export const syncUserCreation=inngest.createFunction(
        event:"clerk/user.created"
     },
     async ({event})=>{
-        const{id, first_name, last_name, email_addresses,Image_Url}=event.data
+        const{id, first_name, last_name, email_addresses,image_url}=event.data
         const userData={
             _id:id,
             name:`${first_name} ${last_name}`,
             email:email_addresses[0].email_address,
-            imageUrl:Image_Url
+            imageUrl:image_url
         }
       await connectDB()
       await User.create(userData)
@@ -37,11 +37,11 @@ export const syncUserUpdation=inngest.createFunction(
        event:"clerk/user.updated"
     },
     async ({event})=>{
-        const{id, first_name, last_name, email_addresses,Image_Url}=event.data
+        const{id, first_name, last_name, email_addresses,image_url}=event.data
         const userData={
             name:`${first_name} ${last_name}`,
             email:email_addresses[0].email_address,
-            imageUrl:Image_Url
+            imageUrl:image_url
         }
       await connectDB()
       await User.findByIdAndUpdate(id, userData)
